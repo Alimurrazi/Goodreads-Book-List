cdad18973440906db729d9289dc9a75f7209056a
Alimur Razi Rana <alimurrazi570@gmail.com>
2023-03-02 12:02:01 +0600

    fix: eslint and prettier extension add

--- server/common/common.routes.config.ts
+++ server/common/common.routes.config.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 
 export class CommonRoutesConfig {
-    app: express.Application;
-    name: string;
-    constructor(app:express.Application, name: string) {
-        this.app = app;
-        this.name = name;
-    }
-    getName() {
-        return this.name
-    }
+  app: express.Application;
+  name: string;
+  constructor(app: express.Application, name: string) {
+    this.app = app;
+    this.name = name;
+  }
+  getName() {
+    return this.name;
+  }
 }
