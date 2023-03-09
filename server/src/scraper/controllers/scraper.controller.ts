543dcaf1bd5745afc3b708b449af4510fcce2efd
Alimur Razi Rana <alimurrazi570@gmail.com>
2023-03-09 09:35:47 +0600

    fix: scraper controller add

--- server/src/scraper/controllers/scraper.controller.ts
+++ server/src/scraper/controllers/scraper.controller.ts
@@ -2,14 +2,37 @@ import axios from 'axios';
 import express from 'express';
 import cheerio from 'cheerio';
 
+interface Book {
+  book: string;
+  author: string;
+  rating: string;
+}
+
 class ScraperController {
   async syncContent(req: express.Request, res: express.Response) {
-    const url = 'https://coinmarketcap.com/';
+    const url = 'https://www.goodreads.com/shelf/show/bengali';
+    //    const selectedElem = 'div.content > div.mainContentContainer > div.mainContent > div.mainContentFloat > div.leftContainer > div.elementList > div.left';
+    const selectedElem = 'div.left';
     axios(url).then(async (response) => {
       const html_data = response.data;
       const $ = cheerio.load(html_data);
-      const users = 'Hello';
-      await res.status(200).send(users);
+      const listedBooks: Book[] = [];
+
+      $(selectedElem).each(async (index, element) => {
+        const bookName = $(element).children('a.bookTitle').text();
+        const author = $(element).children('span.name').text();
+        const ratingInfo = $(element).children('span.greyText').text();
+
+        const book = {
+          book: bookName,
+          author: author,
+          rating: ratingInfo,
+        };
+
+        console.log(bookName);
+        listedBooks.push(book);
+      });
+      await res.status(200).send(listedBooks);
     });
   }
 }
