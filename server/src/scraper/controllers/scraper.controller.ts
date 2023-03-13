9336d6938b2f007f862d1721abf8b50c0edcb864
Alimur Razi Rana <alimurrazi570@gmail.com>
2023-03-13 23:28:16 +0600

    book list new properties add

--- server/src/scraper/controllers/scraper.controller.ts
+++ server/src/scraper/controllers/scraper.controller.ts
@@ -5,7 +5,9 @@ import cheerio from 'cheerio';
 interface Book {
   book: string;
   author: string;
-  rating: string;
+  avgRating: number;
+  ratings: number;
+  firstPublished: number;
 }
 
 class ScraperController {
@@ -16,7 +18,7 @@ class ScraperController {
     //    const promises: Promise<any>[] = [];
 
     while (shouldContinue) {
-      const url = `https://www.goodreads.com/shelf/show/bengali?page=${i}`;
+      const url = `https://www.goodreads.com/shelf/show/favourites?page=${i}`;
       const selectedElem = 'div.left';
       await axios(url, {
         headers: {
@@ -30,19 +32,27 @@ class ScraperController {
           fetchedBooks.each(async (index, element) => {
             if (fetchedBooks.length > 0) {
               const bookName = $(element).children('a.bookTitle').text();
+
               const parentAuthor = $(element).find('span')[1];
               const authorDiv = $(parentAuthor).children('div.authorName__container');
               const authorSpan = $(authorDiv).find('span')[0];
               const author = $(authorSpan).text();
-              const ratingInfo = $(element).children('span.greyText').text();
 
+              const ratingInfo = $(element).children('span.greyText').text();
+              const avgRatingRegex = /avg rating (\d+\.\d+)/;
+              const ratingsRegex = /(\d+(?:,\d+)*) ratings/;
+              const publishedRegex = /published (\d+)/;
+
+              const avgRatingMatch = ratingInfo.match(avgRatingRegex);
+              const ratingsMatch = ratingInfo.match(ratingsRegex);
+              const publishedMatch = ratingInfo.match(publishedRegex);
               const book = {
                 book: bookName,
                 author: author,
-                rating: ratingInfo,
+                avgRating: avgRatingMatch ? parseFloat(avgRatingMatch[1]) : -1,
+                ratings: ratingsMatch ? parseInt(ratingsMatch[1].replace(/,/g, '')) : -1,
+                firstPublished: publishedMatch ? parseInt(publishedMatch[1]) : -1,
               };
-
-              console.log(bookName);
               listedBooks.push(book);
             } else {
               shouldContinue = false;
