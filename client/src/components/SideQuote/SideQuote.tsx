7e765d7506c5b0a6ed664559775fe365d0a8c829
Alimur Razi Rana <alimurrazi570@gmail.com>
2023-05-04 21:08:17 +0600

    feat: side quote align update

--- client/src/components/SideQuote/SideQuote.tsx
+++ client/src/components/SideQuote/SideQuote.tsx
@@ -4,9 +4,11 @@ import './SideQuote.css';
 const SideQuote = () => {
   return (
     <>
-      <div id="quote">
-        <blockquote>So many books, so little time.</blockquote>
-        <cite>Frank Zappa</cite>
+      <div className="flex-column h-100-vh justify-center">
+        <div id="quote">
+          <blockquote>So many books, so little time.</blockquote>
+          <cite>Frank Zappa</cite>
+        </div>
       </div>
     </>
   );
