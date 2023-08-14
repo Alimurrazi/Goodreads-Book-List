6088c9e57316771f4c0bb7acb5d912be40a554bd
Alimur Razi Rana <alimurrazi570@gmail.com>
2023-08-14 23:35:12 +0600

    feat: new method add for jwt validation

--- server/src/auth/middlewares/jwt.middleware.ts
+++ server/src/auth/middlewares/jwt.middleware.ts
@@ -1,4 +1,8 @@
 import express from 'express';
+import { Jwt } from '../../common/types/jwt';
+import jwt from 'jsonwebtoken';
+
+const jwtSecret: string = process.env.JWT_SECRET ? process.env.JWT_SECRET : '';
 
 class JwtMiddleware {
   verifyRefreshBodyField(req: express.Request, res: express.Response, next: express.NextFunction) {
@@ -8,4 +12,23 @@ class JwtMiddleware {
       return res.status(400).send({ errors: 'Missing required fields refereshToken' });
     }
   }
+
+  validJWTNeeded(req: express.Request, res: express.Response, next: express.NextFunction) {
+    if (req.headers['authorization']) {
+      try {
+        const authorization = req.headers['authorization'].split('');
+        if (authorization[0] !== 'Bearer') {
+          return res.status(401).send();
+        } else {
+          res.locals.jwt = jwt.verify(authorization[1], jwtSecret) as Jwt;
+          next();
+        }
+      } catch (error) {
+        return res.status(403).send();
+      }
+    } else {
+      return res.status(401).send();
+    }
+  }
 }
+export default new JwtMiddleware();
